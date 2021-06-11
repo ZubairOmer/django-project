b498117c0f21a5414c1bc3d50f6b3878ef1575a6
Zubair Omer <omer.zubair01@gmail.com>
2021-06-11 10:35:57 -0700

    working on form

--- users/views.py
+++ users/views.py
@@ -6,5 +6,9 @@ from django.contrib import messages
 
 
 def register(request):
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            username =
     form = UserCreationForm
     return render(request, 'users/register.html', {'form': form})
