0db6b0bcc006252c3054fc4af73806f179b57236
Zubair Omer <omer.zubair01@gmail.com>
2021-06-11 10:31:58 -0700

    authentication form created

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('food/', include('food.urls'))
+    path('food/', include('food.urls')),
+    path('register/', views.register, name='register')
 ]
