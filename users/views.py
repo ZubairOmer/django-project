fe59811ad62c43684ab52c02693c8590eddc6c45
Zubair Omer <omer.zubair01@gmail.com>
2021-06-11 16:47:44 -0700

    files checked

--- users/views.py
+++ users/views.py
@@ -12,7 +12,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(
-                request, f'welcome {{username}} your account has been created ')
+                request, f'welcome {username} your account has been created ')
             return redirect('food:index')
     else:
         form = UserCreationForm
